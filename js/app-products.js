221a5ac665d3ecd3bb430b7534ddbca345b95ddb
Omar <omardanielesquivel@gmail.com>
2022-07-27 06:28:59 -0500

    products grid

---
+++ js/app-products.js
@@ -0,0 +1,42 @@
+(() => {
+
+
+    'use strict';
+
+
+    let selectedProduct = -1;
+    const productsVector = document.querySelectorAll('.product');
+
+
+    const activateElement = (element, showClass = '', hiddenClass = '') => {
+        element.classList.toggle(showClass);
+        if (hiddenClass !== '') {
+            element.classList.toggle(hiddenClass);
+        }
+    },
+        showProducts = () => {
+            for (let i = 0; i < productsVector.length; i++) {
+                if (i === selectedProduct) {
+                    continue;
+                } else {
+                    activateElement(productsVector[i], 'product-filter');
+                }
+            }
+        };
+
+    productsVector.forEach((element, index) => {
+        element.addEventListener('mouseover', () => {
+            selectedProduct = index;
+            showProducts();
+        });
+
+        element.addEventListener('mouseout', () => {
+            showProducts();
+            selectedProduct = -1;
+        });
+    });
+
+
+
+
+})();
