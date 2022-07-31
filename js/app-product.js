16556b7e20076707f07c9605ec55766e0c5a2188
Omar <omardanielesquivel@gmail.com>
2022-07-31 06:46:37 -0500

    product page

--- js/app-product.js
+++ js/app-product.js
@@ -4,17 +4,42 @@
     'use strict';
 
 
-    let mainUbication = window.scrollY;
-    
+    let isOver = false;
+    const vectorImages = document.querySelectorAll('.product-detail-galery__figure'),
+    close = document.querySelector('.close-modal-product'),
+    modal = document.querySelector('.image-modal'),
+    modalMainImage = document.querySelector('.modal-main-image'),
+    modalMainLink = document.querySelector('.modal-main-link'),
+    activateElement = (element, showClass = '', hiddenClass = '') => {
+        element.classList.toggle(showClass);
+        if (hiddenClass !== '') {
+            element.classList.toggle(hiddenClass);
+        }
+    };
 
-    window.onscroll = function() {
-        let displacement = window.scrollY;
+
+    modalMainLink.addEventListener('mouseout', () => {
+        isOver = false;
+    });
+
+    modalMainLink.addEventListener('mouseenter', () => {
+        isOver = true;
+    });
+
+    vectorImages.forEach((element) => {
+        element.addEventListener('click', () => {
+            modalMainLink.href = element.childNodes[1].src;
+            modalMainImage.src = element.childNodes[1].src;
+            activateElement(modal, 'image-modal-active');
+        });
+    });
         
-        
-        mainUbication = displacement;
-        console.log(mainUbication);
-        console.log(displacement);
-    }
+    modal.addEventListener('click', () => {
+        if (!isOver) {
+            activateElement(modal, 'image-modal-active');
+
+        }
+    });
 
 
 })();
